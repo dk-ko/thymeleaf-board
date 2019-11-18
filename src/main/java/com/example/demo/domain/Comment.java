768faa6b491f02851a64bddcaa0c9c1029e17e49
dakyung ko <kdkyung57@gmail.com>
2019-11-18 13:03:20 +0900

    feat: Domain class

---
+++ src/main/java/com/example/demo/domain/Comment.java
@@ -0,0 +1,34 @@
+package com.example.demo.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(callSuper = true)
+public class Comment extends BaseEntity implements Serializable {
+
+    @Column
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_comment_user"),
+                nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_comment_article"),
+                nullable = false)
+    private Article article;
+
+    @Builder
+    public Comment(String contents, User user, Article article) {
+        this.contents = contents;
+        this.user = user;
+        this.article = article;
+    }
+}
---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,44 @@
+package com.example.demo.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Entity
+@Table
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString(callSuper = true)
+public class User extends BaseEntity implements Serializable {
+    @Column(nullable = false, unique = true)
+    private String account;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user",
+            cascade = CascadeType.PERSIST,
+            fetch = FetchType.LAZY)
+    List<Comment> commentList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user",
+            cascade = CascadeType.PERSIST,
+            fetch = FetchType.LAZY)
+    List<Article> articleList = new ArrayList<>();
+
+    @Builder
+    public User(String account, String password, List<Comment> commentList, List<Article> articleList) {
+        this.account = account;
+        this.password = password;
+        this.commentList = Optional.ofNullable(commentList).orElse(this.commentList);
+        this.articleList = Optional.ofNullable(articleList).orElse(this.articleList);
+    }
+
+    protected void setPassword(String password) {
+        this.password = password;
+    }
+}

3f882525090c2b59ef0f4bd57f13803096cce276
dakyung ko <kdkyung57@gmail.com>
2019-11-18 13:03:20 +0900

    feat: Domain class

---
+++ src/main/java/com/example/demo/domain/Board.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(callSuper = true)
+public class Board extends BaseEntity implements Serializable {
+    @Column(nullable = false, unique = true)
+    String name;
+
+    @OneToMany(mappedBy = "board",
+                cascade = CascadeType.ALL,
+                fetch = FetchType.LAZY, // TODO EAGER ? > 우선 개발 진행해보고 수정
+                orphanRemoval = true)
+    List<Article> articleList = new ArrayList<>();
+
+    @Builder
+    public Board(String name, List<Article> articleList) {
+        this.name = name;
+        this.articleList = Optional.ofNullable(articleList).orElse(this.articleList);
+    }
+}

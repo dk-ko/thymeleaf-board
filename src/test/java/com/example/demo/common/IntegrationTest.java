2d19a9d1101f75d38d5ade1ef0f8991155a8cd9a
고다경 <kodakyung@estgames.com>
2019-11-21 13:31:01 +0900

    test: Article paging

---
+++ src/test/java/com/example/demo/common/IntegrationTest.java
@@ -0,0 +1,19 @@
+package com.example.demo.common;
+
+import com.example.demo.common.type.TestProfile;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles(TestProfile.TEST)
+@Transactional
+@Ignore
+public class IntegrationTest {
+}

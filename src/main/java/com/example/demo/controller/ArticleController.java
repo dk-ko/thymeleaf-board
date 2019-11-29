e89061163c3e57c74276b792be82c837441bf752
고다경 <kodakyung@estgames.com>
2019-11-29 17:48:14 +0900

    feat: Article Controller

---
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -0,0 +1,56 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.User;
+import com.example.demo.dto.req.ArticleCreateReqDto;
+import com.example.demo.dto.req.ArticleUpdateReqDto;
+import com.example.demo.dto.res.ArticleResDto;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.ArticleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@AllArgsConstructor
+@Api(value = "Article(게시글)")
+public class ArticleController {
+    private final ArticleService articleService;
+    private final UserRepository userRepository;
+
+    @ApiOperation("게시글 조회")
+    @GetMapping("/articles/{articleIdx}") // todo url에 board가 있는게 좋을까..? "board/1/2", "board/1/article/2" // 아니면 아예 url에 board를 없애고 articlea만? "article/2"
+    public String getArticle(/*@PathVariable final Long boardIdx,*/ @PathVariable final Long articleIdx, Model model) {
+        ArticleResDto article = articleService.getArticle(articleIdx);
+        model.addAttribute("article", article);
+        return "/article/contents";
+    }
+
+    @ApiOperation("게시글 생성")
+    @PostMapping("/boards/{boardIdx}")
+    public String createArticle(@PathVariable final Long boardIdx, ArticleCreateReqDto articleCreateReqDto, Model model) {// todo test user
+        ArticleResDto article = articleService.createArticle(articleCreateReqDto, testUser(), boardIdx);
+        model.addAttribute("article", article);
+        return "redirect:articles/" + article.getArticleIdx(); // todo 생성한 '게시글 조회'로 이동
+    }
+
+    @ApiOperation("게시글 삭제")
+    @DeleteMapping("/articles/{articleIdx}")
+    public String deleteArticle(@PathVariable final Long articleIdx) {
+        Long returnBoardIdx = articleService.deleteArticle(articleIdx, testUser());
+        return "redirect:boards/" + returnBoardIdx; // todo "게시판의 게시글 목록 조회"로 돌아가기
+    }
+
+    @ApiOperation("게시글 수정")
+    @PutMapping("/articles/{articleIdx}")
+    public String editArticle(@PathVariable final Long articleIdx, ArticleUpdateReqDto articleUpdateReqDto) {
+        ArticleResDto editArticle = articleService.editArticle(articleIdx, testUser(), articleUpdateReqDto);
+        return "redirect:articles/" + editArticle.getArticleIdx(); // todo 수정한 '게시글 조회'로 이동
+    }
+
+    private User testUser() { // todo security 구현 후 삭제
+        return userRepository.findById(1L).get();
+    }
+}
